f550171a56453f810848d6a878d6707cee4b7e2f
OddNomadic <handelandc@gmail.com>
2023-08-24 23:02:36 -0600

    install custom NVChad config

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# CamConf
+# OddConf
+A collection of scripts and pre-made config files that will seup a very particular environment.
--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 # -------------------------
 
 # install snap
-apt update
-apt install snapd
+sudo apt update
+sudo apt install snapd
 
-snap install nvim --classic
-snap install tmux --classic
+sudo snap install nvim --classic
+sudo snap install tmux --classic
 
 # install tmux plugin manager
 # https://github.com/tmux-plugins/tpm
@@ -22,11 +22,13 @@ cp ~/.tmux.conf ~/.tmux.conf.bak
 cp ./tmux.conf ~/.tmux.conf
 
 # Install NVChad
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+#git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+mkdir -p ~/.config/nvim
+cp -r nvim ~/.config/nvim
 
 # TODO override some of NVChad's files with my own
 
 # .bashrc
 # _________________________
 echo "alias vim='nvim'" >> ~/.bashrc
-echo "export TERM=xterm" >> ~/.bahsrc # this fixed a tmux problem on one machine 
+#echo "export TERM=xterm" >> ~/.bahsrc # this fixed a tmux problem on one machine 
